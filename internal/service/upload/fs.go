f2bbb65d526fb64ffcbf8a8e0094afd6bb5a1e78
KNU-K <knukang334@gmail.com>
2025-02-23 21:52:44 +0900

    feat: 파일 저장 시 랜덤 파일 이름 생성 기능 추가

--- internal/service/upload/fs.go
+++ internal/service/upload/fs.go
@@ -1,29 +1,44 @@
 package upload_service
 
 import (
+	"crypto/rand"
+	"fmt"
 	"io"
 	"mime/multipart"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 func SaveToFS(file multipart.File, handler *multipart.FileHeader) error {
-	uploadDir := filepath.Join(".", "uploads")
-	if _, err := os.Stat(uploadDir); os.IsNotExist(err) {
-		if err := os.Mkdir(uploadDir, os.ModePerm); err != nil {
-			return err
-		}
-	}
+    uploadDir := filepath.Join(".", "uploads")
+    if _, err := os.Stat(uploadDir); os.IsNotExist(err) {
+        if err := os.Mkdir(uploadDir, os.ModePerm); err != nil {
+            return err
+        }
+    }
 
-	dst, err := os.Create(filepath.Join(uploadDir, handler.Filename))
-	if err != nil {
-		return err
-	}
-	defer dst.Close()
+    // Generate a random filename
+    randomFilename := generateRandomFilename(handler.Filename)
 
-	if _, err := io.Copy(dst, file); err != nil {
-		return err
-	}
+    dst, err := os.Create(filepath.Join(uploadDir, randomFilename))
+    if err != nil {
+        return err
+    }
+    defer dst.Close()
 
-	return nil
+    if _, err := io.Copy(dst, file); err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func generateRandomFilename(originalFilename string) string {
+    timestamp := time.Now().Format("20060102150405")
+    randomBytes := make([]byte, 8)
+    rand.Read(randomBytes)
+    randomString := fmt.Sprintf("%x", randomBytes)
+    extension := filepath.Ext(originalFilename)
+    return fmt.Sprintf("%s_%s%s", timestamp, randomString, extension)
 }
